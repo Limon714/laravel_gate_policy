e14c9ffbcfca0190c42821a4f14d47ddaa22b6d6
Limon Islam <72975868+Limon714@users.noreply.github.com>
2022-09-26 23:58:21 +0600

    Update index.js

--- Project1/index.js
+++ Project1/index.js
@@ -4,7 +4,7 @@
 var nameToImdb = require("name-to-imdb");
 nameToImdb("south park", function (err, res, inf) {
   console.log(res); // "tt0121955"
-  // inf contains info on where we matched that name - e.g. metadata, or on imdb
+  // inf contains info on where we matched that name - eg. metadata, or on imdb
   // and the meta object with all the available data
   console.log(inf);
 });
