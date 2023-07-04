32d855c3f8164f6eb35cfc531b55757a892a5b0c
Limon <limon4u714@gmail.com>
2023-07-05 01:08:11 +0600

    class with ts

--- output/script.js
+++ output/script.js
@@ -1,11 +1,18 @@
 "use strict";
-let calculation;
-calculation = (a, b, c) => {
-    if (c === "add") {
-        return a + b;
+class Player {
+    constructor(n, a, r) {
+        this.name = n;
+        this.age = a;
+        this.role = r;
     }
-    else {
-        return a - b;
+    play() {
+        console.log(`${this.name} is ${this.age} good at ${this.role}`);
     }
-};
-console.log(calculation(91, 75, "minus"));
+}
+const rana = new Player("Rana", 30, " Batting");
+const Rahim = new Player("Ar.Rahim", 30, " Batting");
+const Players = [];
+Players.push(rana);
+Players.push(Rahim);
+console.log(rana.age);
+console.log(Rahim.role);
--- src/script.ts
+++ src/script.ts
@@ -1,12 +1,25 @@
-let calculation : (a: number, b:number, c:string)=> number;
+class Player {
+name : string;
+age: number;
+role:string;
 
-calculation =(a: number, b:number, c:string)=>{
-    if(c === "add"){
-        return a + b ;
-    }else{
-        return a - b ;
-    }
-    
+constructor(n:string, a:number, r:string){
+    this.name = n;
+    this.age = a;
+    this.role = r;
 }
 
-console.log(calculation(91, 75, "minus"));
+play (){
+    console.log(`${this.name} is ${this.age} good at ${this.role}`)
+}
+}
+const rana = new Player("Rana", 30, " Batting");
+const Rahim = new Player("Ar.Rahim", 30, " Batting");
+
+const Players : Player[] = [];
+
+Players.push(rana)
+Players.push(Rahim)
+
+console.log(rana.age)
+console.log(Rahim.role)
